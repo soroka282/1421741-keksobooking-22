86f4d35520663395b0fcb57bc9ad4f964affd45b
soroka282 <soroka282@yandex.ru>
2021-01-31 19:17:32 +0300

    Доработки

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 // Функция, возвращающая случайное целое число;
 const getRandomValue = (min, max) => {
-  if (min < -1 || max < 0 || min >= max) {
-    return 'Error';
+  if (min < 0 || max < 0 || min >= max) {
+    throw new Error('Ошибка');
   }
   return Math.round(Math.random() * (max - min) + min);
 };
 
-(getRandomValue(0, 3));
+getRandomValue(0, 2);
 
 // Функция, возвращающая случайное число с плавающей точкой;
 const getValueFloatPoint = (min, max, placePoint) => {
   if (min < 0 || max < 0 || min >= max) {
-    return 'Error';
+    throw new Error('Ошибка');
   }
   let val = Math.random() * (max - min) + min;
   return val.toFixed(placePoint);
 };
 
-(getValueFloatPoint(1, 3, 2));
+getValueFloatPoint(1, 3, 2);
