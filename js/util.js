5c6e02a40a124dcc4c1f065cc0e9fbbca4c19937
soroka282 <soroka282@yandex.ru>
2021-02-11 16:50:24 +0300

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,22 @@
+export {getRandomValue, getValueFloatPoint};
+
+// Функция, возвращающая случайное число
+const getRandomValue = (min, max) => {
+  if (min < 0 || max < 0 || min >= max) {
+    throw new Error('Ошибка');
+  }
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+getRandomValue(0, 2);
+
+// Функция, возвращающая случайное число с плавающей точкой;
+const getValueFloatPoint = (min, max, placePoint) => {
+  if (min < 0 || max < 0 || min >= max) {
+    throw new Error('Ошибка');
+  }
+  let val = Math.random() * (max - min) + min;
+  return val.toFixed(placePoint);
+};
+
+getValueFloatPoint(1, 3, 2);
