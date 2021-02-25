cf0fafaff29d6db7bd4a903a4284f92e36054788
soroka282 <soroka282@yandex.ru>
2021-02-25 18:56:01 +0300

    Помощь друга (часть 2)

--- js/form.js
+++ js/form.js
@@ -61,6 +61,109 @@ const setCoordinates = (variables, evt) => {
   variables.value = ` ${(evt.target.getLatLng().lat).toFixed(placePoint)} , ${(evt.target.getLatLng().lng).toFixed(placePoint)}`;
 };
 
+// валидация поля заголовка объявления
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+
+const titleInputForm = document.querySelector('#title');
+
+titleInputForm.addEventListener('input', () => {
+  const valueLength = titleInputForm.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    titleInputForm.setCustomValidity(`Ещё минимум ${(MIN_TITLE_LENGTH - valueLength)} симв.`);
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    titleInputForm.setCustomValidity(`Удалите лишние ' ${(valueLength - MAX_TITLE_LENGTH)} симв.`);
+  } else {
+    titleInputForm.setCustomValidity('');
+  }
+  titleInputForm.reportValidity();
+});
+
+//валидация поля "цена за ночь"
+const MAX_PRICE_VALUE = 1000000;
+const MIN_PRICE_VALUE = 0;
+
+priceHousingInput.addEventListener('input', () => {
+  if (priceHousingInput.value > MAX_PRICE_VALUE) {
+    priceHousingInput.setCustomValidity(`Максимум ${MAX_PRICE_VALUE}`);
+  } else if (priceHousingInput.value < MIN_PRICE_VALUE) {
+    priceHousingInput.setCustomValidity(`Отрицательные числа не допускаются. Минимум ${MIN_PRICE_VALUE}`);
+  } else {
+    priceHousingInput.setCustomValidity('');
+  }
+  priceHousingInput.reportValidity();
+});
+
+//валидация поля "колчисетво комнат и количество мест"
+const CapacityOptionValue = {
+  threeGuest : 3,
+  twoGuest : 2,
+  oneGuest : 1,
+  notGuest : 0,
+}
+const RoomNumberValue = {
+  oneRoom : 1,
+  twoRoom : 2,
+  threeRoom : 3,
+  hundredRoom : 100,
+}
+
+const roomNumberSelect = document.querySelector('#room_number');
+const capacitySelect = document.querySelector('#capacity');
+
+//функция для добавления атрибута disabled option
+const addAttributeDisabled = (attribute, selectElem) => {
+  attribute[selectElem].setAttribute('disabled', '');
+};
+
+//функция для удаления атрибута disabled option
+const removeAttributeDisabled = (attribute, selectElem) => {
+  attribute[selectElem].removeAttribute('disabled');
+};
+
+//значение по умолчанию при открытии страницы для выбранных селекторов
+if (roomNumberSelect.value == RoomNumberValue.oneRoom) {
+  capacitySelect.value = roomNumberSelect.value;
+  addAttributeDisabled(capacitySelect, CapacityOptionValue.notGuest);
+  addAttributeDisabled(capacitySelect, CapacityOptionValue.oneGuest);
+  addAttributeDisabled(capacitySelect, CapacityOptionValue.threeGuest);
+  removeAttributeDisabled(capacitySelect, CapacityOptionValue.twoGuest);
+}
+
+//валидация поля в зависимости от выбранных параметров
+roomNumberSelect.addEventListener('input', () => {
+
+  if (roomNumberSelect.value == RoomNumberValue.hundredRoom) {
+    capacitySelect.selectedIndex = roomNumberSelect.selectedIndex;
+    addAttributeDisabled(capacitySelect, CapacityOptionValue.oneGuest);
+    addAttributeDisabled(capacitySelect, CapacityOptionValue.twoGuest);
+    addAttributeDisabled(capacitySelect, CapacityOptionValue.notGuest);
+    removeAttributeDisabled(capacitySelect, CapacityOptionValue.threeGuest);
+
+  } else if (roomNumberSelect.value == RoomNumberValue.oneRoom) {
+    capacitySelect.value = roomNumberSelect.value;
+    addAttributeDisabled(capacitySelect, CapacityOptionValue.notGuest);
+    addAttributeDisabled(capacitySelect, CapacityOptionValue.oneGuest);
+    addAttributeDisabled(capacitySelect, CapacityOptionValue.threeGuest);
+    removeAttributeDisabled(capacitySelect, CapacityOptionValue.twoGuest);
+
+  } else if (roomNumberSelect.value == RoomNumberValue.twoRoom) {
+    capacitySelect.value = roomNumberSelect.value;
+    addAttributeDisabled(capacitySelect, CapacityOptionValue.notGuest);
+    addAttributeDisabled(capacitySelect, CapacityOptionValue.threeGuest);
+    removeAttributeDisabled(capacitySelect, CapacityOptionValue.twoGuest);
+    removeAttributeDisabled(capacitySelect, CapacityOptionValue.oneGuest);
+
+  } else if (roomNumberSelect.value == RoomNumberValue.threeRoom) {
+    capacitySelect.value = roomNumberSelect.value;
+    addAttributeDisabled(capacitySelect, CapacityOptionValue.threeGuest);
+    removeAttributeDisabled(capacitySelect, CapacityOptionValue.twoGuest);
+    removeAttributeDisabled(capacitySelect, CapacityOptionValue.oneGuest);
+    removeAttributeDisabled(capacitySelect, CapacityOptionValue.notGuest);
+  }
+});
+
 export {
   adForm,
   mapFilters,
